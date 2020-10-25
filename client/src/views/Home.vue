b76001b25d1824993ade3761c004dd59cf9886af
bagasss19 <bagasgustip19@gmail.com>
2020-10-25 22:05:12 +0700

    add frontend

---
+++ client/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <section id ="nav">
+    <h1 class="text-center text-white">Silahkan Masukkan Biodata Kamu</h1>
+    <form action="/action_page.php">
+      <label for="fname">Nama Lengkap:</label><br>
+      <input type="text" id="fname" name="fname" placeholder="masukan nama kamu"><br>
+      <label for="lname">Umur:</label><br>
+      <input type="number" id="lname" name="lname" placeholder="masukan umur kamu"><br><br>
+      <input type="submit" value="Submit">
+    </form>
+  </section>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
